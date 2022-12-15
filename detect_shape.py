985fde887eed62e303f9ab55fcebcfa86dcc358c
KTMMMM <110892552+KTMMMM@users.noreply.github.com>
2022-12-15 20:14:07 +0900

    tm

--- detect_shape.py
+++ detect_shape.py
@@ -22,7 +22,7 @@ def getAngle(p1,p2,p3, direction='CW'):
     return result
 
 def distance(p1, p2):
-    result = math.sqrt((p1[0]-p2[0])**2 + (p1[0]-p2[0])**2)
+    result = math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
     return result
 
 def extract_facedata(path):
